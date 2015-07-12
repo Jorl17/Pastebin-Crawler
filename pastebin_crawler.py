d74fce20a5adb8673dd228811350f9020a10db85
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-07-12 01:03:56 +0100

    Adding GPLv2 notice

--- pastebin_crawler.py
+++ pastebin_crawler.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
+##
+## Copyright (C) 2015 João Ricardo Lourenço <jorl17.8@gmail.com>
+##
+## Github: https://github.com/Jorl17
+## Project main repository: https://github.com/jorl17/Pastebin-Crawler
+##
+## (original source in public domain found in
+## https://github.com/FabioSpampinato/Pastebin-Crawler, by
+## Fabio Spampinato. This fork has since been merged back.)
+##
+##
+## This file is part of Pastebin-Crawler.
+##
+## Pastebin-Crawler is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## Pastebin-Crawler is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Pastebin-Crawler.  If not, see <http://www.gnu.org/licenses/>.
+##
 from math import ceil
 from optparse import OptionParser
 import os
